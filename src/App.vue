c7e43fa441f93f365d5ed514e94d666d1b6d1af2
牛力新 <eric890@163.com>
2018-02-27 10:52:28 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>这是公共顶部</h1>
+    <router-link to="/outter/register">注册</router-link>
+    <router-view/>
+    <h1>这是公共底部</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Outter.vue
@@ -0,0 +1,61 @@
+<template>
+  <div >
+    <h3 v-bind:class="style">欢迎{{reverseInfo}}----{{info}}</h3>
+    <p v-show="b">这是一段文字{{reverseInfo}}</p>
+    <p>{{fullName}}</p>
+    <button v-on:click="foo">按钮</button>
+    <p>我被点击了{{count}}次</p>
+    <input type="text" v-model="info">
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      info:'注册',
+      style:'test',
+      n:1,
+      b:1,
+      fristName:'文飞',
+      lastName:'胡',
+      count:0
+    }
+  },//data:function(){return ....}
+  methods:{
+    foo:function(event){
+        // this.info = '登录';
+        this.fullName = '振华 梁';
+        this.b = false;
+        console.log(event.target);
+      }
+  },
+  computed:{
+    reverseInfo(){
+      console.log('run in this');
+      return this.info.split('').reverse().join('');
+    },
+    fullName:{
+      get(){
+        return this.fristName+' '+this.lastName;
+      },
+      set(newValue){
+        var names = newValue.split(' ');
+        this.fristName = names[0];
+        this.lastName = names[names.length-1];
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  .test{
+    color:red;
+  }
+</style>
